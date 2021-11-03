6bc07a5c22b91d1eeac0a2cd4bbf54c53e5ae597
Gisha <spaceshhz@gmail.com>
2021-11-03 16:33:58 +0200

    create player simple jump

--- GameOff-2021/Assets/_Project/Scripts/PlayerMovement.cs
+++ GameOff-2021/Assets/_Project/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ namespace Gisha.GameOff_2021
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private float jumpForce = 10f;
 
         private float _hInput;
 
@@ -19,11 +20,19 @@ namespace Gisha.GameOff_2021
         private void Update()
         {
             _hInput = Input.GetAxis("Horizontal");
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                Jump();
         }
 
         private void FixedUpdate()
         {
-            _rb.velocity = transform.right * _hInput * moveSpeed;
+            _rb.velocity = new Vector2(_hInput * moveSpeed, _rb.velocity.y);
+        }
+
+        private void Jump()
+        {
+            _rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 }
