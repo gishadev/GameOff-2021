689cb790792d77a0a4b19588f5acbbaa9d8c1083
Gisha <spaceshhz@gmail.com>
2021-11-03 16:28:48 +0200

    create simple horizontal movement

--- GameOff-2021/Assets/_Project/Scripts/PlayerMovement.cs
+++ GameOff-2021/Assets/_Project/Scripts/PlayerMovement.cs
@@ -1,9 +1,29 @@
+using System;
 using UnityEngine;
 
 namespace Gisha.GameOff_2021
 {
     public class PlayerMovement : MonoBehaviour
     {
+        [SerializeField] private float moveSpeed = 5f;
 
+        private float _hInput;
+
+        private Rigidbody2D _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            _hInput = Input.GetAxis("Horizontal");
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.velocity = transform.right * _hInput * moveSpeed;
+        }
     }
 }
