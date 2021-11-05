bb91939a0b7c73ac0af4a12d38883d507421a618
Gisha <spaceshhz@gmail.com>
2021-11-05 18:16:37 +0200

    add camera follow contoller

---
+++ GameOff-2021/Assets/_Project/Scripts/CameraFollowController.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace Gisha.GameOff_2021
+{
+    public class CameraFollowController : MonoBehaviour
+    {
+        [SerializeField] private Transform followTarget;
+        [SerializeField] private float followSpeed;
+
+        private void FixedUpdate()
+        {
+            Vector3 newPosition = new Vector3(followTarget.position.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPosition, followSpeed * Time.deltaTime);
+        }
+    }
+}
