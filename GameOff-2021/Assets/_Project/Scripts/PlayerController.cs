b3d3f784affd894580fc36848dd54948ffaccb5a
Gisha <spaceshhz@gmail.com>
2021-11-05 16:44:22 +0200

    improve player movement: add ground checking

---
+++ GameOff-2021/Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+using System;
+using UnityEngine;
+
+namespace Gisha.GameOff_2021
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 500f;
+        [SerializeField] private float jumpForce = 8f;
+
+        [Header("Ground Checker")] [Space] [SerializeField]
+        private Transform groundCheckerPoint;
+
+        [SerializeField] private float groundCheckerRadius = 0.25f;
+
+        private LayerMask _groundLayer;
+        private bool _isGrounded = true;
+        private float _hInput;
+
+        private Rigidbody2D _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _groundLayer = 1 << LayerMask.NameToLayer("Ground");
+        }
+
+        private void Update()
+        {
+            HandleInput();
+            CheckForGround();
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.velocity = new Vector2(_hInput * moveSpeed * Time.deltaTime, _rb.velocity.y);
+        }
+
+        private void HandleInput()
+        {
+            _hInput = Input.GetAxis("Horizontal");
+
+            if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+                Jump();
+        }
+
+        private void Jump()
+        {
+            _rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        private void CheckForGround()
+        {
+            _isGrounded = Physics2D.OverlapCircle(groundCheckerPoint.position, groundCheckerRadius, _groundLayer);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheckerPoint.position, groundCheckerRadius);
+        }
+    }
+}
