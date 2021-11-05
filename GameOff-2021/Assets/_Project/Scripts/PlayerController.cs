9ffd70e253142423d1274966c3fe3a5ccf4f0056
Gisha <spaceshhz@gmail.com>
2021-11-05 17:38:02 +0200

    add coyote time

--- GameOff-2021/Assets/_Project/Scripts/PlayerController.cs
+++ GameOff-2021/Assets/_Project/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Gisha.GameOff_2021
@@ -7,12 +8,15 @@ namespace Gisha.GameOff_2021
     {
         [SerializeField] private float moveSpeed = 500f;
         [SerializeField] private float jumpForce = 8f;
+        [SerializeField] private float coyoteTime = 0.2f;
 
-        [Header("Ground Checker")] [Space] [SerializeField]
+        [Header("Ground Checker")] [SerializeField]
         private Transform groundCheckerPoint;
 
         [SerializeField] private float groundCheckerRadius = 0.25f;
+        [SerializeField] private float afterJumpCheckDelay = 0.1f;
 
+        private float _coyoteCounter;
         private LayerMask _groundLayer;
         private bool _isGrounded = true;
         private float _hInput;
@@ -25,10 +29,14 @@ namespace Gisha.GameOff_2021
             _groundLayer = 1 << LayerMask.NameToLayer("Ground");
         }
 
+        private void Start()
+        {
+            StartCoroutine(GroundCheckCoroutine());
+        }
+
         private void Update()
         {
             HandleInput();
-            CheckForGround();
         }
 
         private void FixedUpdate()
@@ -40,18 +48,46 @@ namespace Gisha.GameOff_2021
         {
             _hInput = Input.GetAxis("Horizontal");
 
+            // Coyote counter.
+            if (_isGrounded)
+                _coyoteCounter = coyoteTime;
+            else
+                _coyoteCounter -= Time.deltaTime;
+
             // Making full jump.
-            if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
-                _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
+            if (Input.GetKeyDown(KeyCode.Space) && _coyoteCounter > 0)
+            {
+                MakeFullJump();
+                StopAllCoroutines();
+                StartCoroutine(GroundCheckCoroutineWithDelay(afterJumpCheckDelay));
+            }
 
             // Making half jump.
             if (Input.GetKeyUp(KeyCode.Space) && _rb.velocity.y > 0)
                 _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y * 0.5f);
         }
 
-        private void CheckForGround()
+        private void MakeFullJump()
         {
-            _isGrounded = Physics2D.OverlapCircle(groundCheckerPoint.position, groundCheckerRadius, _groundLayer);
+            _isGrounded = false;
+            _coyoteCounter = 0;
+
+            _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
+        }
+
+        private IEnumerator GroundCheckCoroutine()
+        {
+            while (true)
+            {
+                _isGrounded = Physics2D.OverlapCircle(groundCheckerPoint.position, groundCheckerRadius, _groundLayer);
+                yield return null;
+            }
+        }
+
+        private IEnumerator GroundCheckCoroutineWithDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            StartCoroutine(GroundCheckCoroutine());
         }
 
         private void OnDrawGizmos()
