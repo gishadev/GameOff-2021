53757bfc25ffc3f916b47d30dba6e8cee1bdc8f5
Gisha <gishathedev@gmail.com>
2021-11-09 21:34:38 +0200

    add parallax effect & lamps flickering

---
+++ GameOff-2021/Assets/_Project/Scripts/ParallaxEffect.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Gisha.GameOff_2021
+{
+    public class ParallaxEffect : MonoBehaviour
+    {
+        [SerializeField] private float parallaxSpeed = 2f;
+        
+        private Material _mat;
+        private float _xOffset = 0;
+
+        private void Awake()
+        {
+            _mat = GetComponent<MeshRenderer>().material;
+        }
+
+        private void Update()
+        {
+            _xOffset += parallaxSpeed * Time.deltaTime;
+            _mat.mainTextureOffset = transform.right * _xOffset;
+        }
+    }
+}
