fe91973bfbba379cac45e871c65c1e593672dcb8
Gisha <gishathedev@gmail.com>
2021-11-09 21:21:30 +0200

    add camera target controller

---
+++ GameOff-2021/Assets/_Project/Scripts/CameraTargetController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Gisha.GameOff_2021
+{
+    /// <summary>
+    /// This script should be attached to the player.
+    /// </summary>
+    public class CameraTargetController : MonoBehaviour
+    {
+        [SerializeField] private float aheadAmount, aheadSpeed;
+
+        private void Update()
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                float xPos = Mathf.Lerp(transform.localPosition.x, aheadAmount * Input.GetAxisRaw("Horizontal"),
+                    Time.deltaTime * aheadSpeed);
+
+                transform.localPosition = new Vector3(xPos, transform.position.y,
+                    transform.position.z);
+            }
+        }
+    }
+}
