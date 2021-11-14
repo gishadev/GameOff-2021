05df795e29c4f5c8c9d2532f37b0a477ccafd107
Gisha <gishathedev@gmail.com>
2021-11-14 23:26:18 +0200

    add max and min height for elevator

--- GameOff-2021/Assets/_Project/Scripts/Interactive/Elevator.cs
+++ GameOff-2021/Assets/_Project/Scripts/Interactive/Elevator.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Gisha.GameOff_2021.Interactive
@@ -5,16 +6,30 @@ namespace Gisha.GameOff_2021.Interactive
     public class Elevator : Controllable, IUITwoAxisControl
     {
         [SerializeField] private float speed;
+        [SerializeField] private float maxHeight, minHeight;
 
         private bool _isWorking = false;
         private float _yDirection = 0f;
+        private Vector3 _startPos;
+        private float _maxAbsHeight, _minAbsHeight;
+
+        private void Awake()
+        {
+            _startPos = transform.position;
+            _maxAbsHeight = _startPos.y + maxHeight;
+            _minAbsHeight = _startPos.y + minHeight;
+        }
 
         private void Update()
         {
             if (!_isWorking)
                 return;
 
-            transform.Translate(Vector2.up * speed * _yDirection * Time.deltaTime);
+            var yDir = transform.position.y + 0.1f * _yDirection;
+            if (yDir < _maxAbsHeight && yDir > _minAbsHeight)
+                transform.Translate(Vector2.up * speed * _yDirection * Time.deltaTime);
+            else
+                Stop();
         }
 
         protected override void OnAddInteractActions()
@@ -45,6 +60,22 @@ namespace Gisha.GameOff_2021.Interactive
         {
             _isWorking = false;
             _yDirection = 0f;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!Application.isPlaying)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(transform.position + Vector3.up * maxHeight, 0.5f);
+                Gizmos.DrawWireSphere(transform.position + Vector3.up * minHeight, 0.5f);
+            }
+            else
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawWireSphere(_startPos + Vector3.up * maxHeight, 0.5f);
+                Gizmos.DrawWireSphere(_startPos + Vector3.up * minHeight, 0.5f);
+            }
         }
     }
 }
